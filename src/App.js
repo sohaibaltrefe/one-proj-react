f62ecf68d9b2f35d93ee20d2eabc6dfa5b0651a5
sohaibaltrefe <sohaibshaltafaltrefe1@gmail.com>
2023-10-24 18:16:18 +0300

    proj

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+
+export default function App() {
+  return (
+    <p>hi sohaib </p>
+  );
+}
+
---
+++ src/Navbar.js
@@ -0,0 +1,6 @@
+
+export default function Navbar() {
+  return (
+<h1 className=" alert alert-danger">sdsad</h1>
+    )
+}
